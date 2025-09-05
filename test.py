1cd4ac97066f664b4a615c9a01e870a74969577e
Siru <ehdgur9553@naver.com>
2025-09-05 15:40:37 +0900

    issue2

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-print('hello world')
+for i in range(5):
+    print(i)
+    print('number:', i)
 
 for i in range(5):
     print(i)
     print('looping:', i)
     if i % 2 == 0:
-        print('even number')
+        print('even number:', i)
     else:
-        print('odd number')
+        print('odd number:', i)
